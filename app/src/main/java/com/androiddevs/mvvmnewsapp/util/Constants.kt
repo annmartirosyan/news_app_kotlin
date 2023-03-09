0b5b0c1ce19de063edb9e39847ed3eb5415df39d
Anna <anna.martirosyan@powerdata.de>
2023-03-09 22:27:08 +0400

    Retrofit Setup

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "19660c5e80044aa5b6fd0e85b3377907"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
