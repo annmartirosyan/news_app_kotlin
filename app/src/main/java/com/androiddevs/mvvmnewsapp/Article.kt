d0df01b90c8f5daecff45785efe6924579aec646
Anna <anna.martirosyan@powerdata.de>
2023-03-09 22:39:54 +0400

    DAO and Article Setup

--- app/src/main/java/com/androiddevs/mvvmnewsapp/Article.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/Article.kt
@@ -1,8 +1,17 @@
 package com.androiddevs.mvvmnewsapp
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+
+@Entity(
+    tableName = "articles"
+)
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    var  id: Int? = null,
+
     @SerializedName("author")
     val author: String,
     @SerializedName("content")
