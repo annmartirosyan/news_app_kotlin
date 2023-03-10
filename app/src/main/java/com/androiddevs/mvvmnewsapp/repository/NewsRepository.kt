6c17fde66021431333295bff457ddd70229bb698
anna.martirosyan <anna.martirosyan@softconstruct.com>
2023-03-10 16:53:47 +0400

    Adding and Deleting Articles to our Room Database

--- app/src/main/java/com/androiddevs/mvvmnewsapp/repository/NewsRepository.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/repository/NewsRepository.kt
@@ -2,6 +2,7 @@ package com.androiddevs.mvvmnewsapp.repository
 
 import com.androiddevs.mvvmnewsapp.api.RetrofitInstance
 import com.androiddevs.mvvmnewsapp.db.ArticleDatabase
+import com.androiddevs.mvvmnewsapp.models.Article
 import com.androiddevs.mvvmnewsapp.models.NewsResponse
 import com.androiddevs.mvvmnewsapp.util.Resource
 import retrofit2.Response
@@ -16,6 +17,10 @@ class NewsRepository(
     suspend fun searchNews (searchQuery: String, pageNumber: Int)=
         RetrofitInstance.api.searchForNews(searchQuery, pageNumber)
 
+    suspend fun upsert(article: Article) = db.getArticleDao().upsert(article)
 
+    fun getSavedNews() = db.getArticleDao().getAllArticles()
+
+    suspend fun deleteArticle(article: Article) = db.getArticleDao().deleteArticle(article)
 
 }
